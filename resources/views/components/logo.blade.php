072cae7417a043017c62d6b0c269c5819387919e
MARIEEE0528 <mariecatugas1@gmail.com>
2024-05-03 01:32:32 +0800

    ano ba to

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,3 +1,10 @@
-<div class="text-3xl">
-  <span class="text-primary-500 font-bold">ALUMNI PORTAL</span>
+<img src="{{ asset('/images/logo only.png') }}" alt="Logo" class="h-12">
+<div class = "w-4/5 ml-15 flex flex-col">
+  <h1 class="text-15 font-bold -mb-2 text-primary-500">
+        PAMANTASAN NG LUNGSOD NG MAYNILA
+  </h1>
+  <h2 class="text-[15px] font-inter">
+        UNIVERSITY OF THE CITY OF MANILA
+  </h2>
 </div>
+
