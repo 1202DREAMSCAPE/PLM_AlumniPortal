5577273969215cc0c02b97358733f5452d6e511b
MARIEEE0528 <mariecatugas1@gmail.com>
2024-05-21 16:28:51 +0800

    remove x

--- app/Livewire/LoginButton.php
+++ app/Livewire/LoginButton.php
@@ -5,7 +5,7 @@ namespace App\Livewire;
 use Livewire\Component;
 
 class LoginButton extends Component
-{x
+{
     public function redirectToLogin()
     {
         return redirect()->route('alumni');
