42c838971a53d025c5e52da719f1a3a37de7aad8
Seajay <cjmcerezo2021@plm.edu.ph>
2024-05-22 08:23:52 +0800

    changes more

--- resources/views/Alumni Gallery.blade.php
+++ resources/views/Alumni Gallery.blade.php
@@ -1,35 +1,51 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Alumni Gallery</title>
-    <link rel="stylesheet" href="/dist/app.css">
-    <link rel="stylesheet" href="/dist/output.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Katibeh:wght@400&display=swap" />
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.3/dist/cdn.min.js"></script>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alumni Portal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css">
+  <link rel="stylesheet" href="/dist/app.css">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.3/dist/cdn.min.js"></script>
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Katibeh:wght@400;700&display=swap');
+  </style>
 </head>
 
-    <body class="bg-white-10 flex flex-col flex-nowrap">
+<body class="bg-white-10 h-screen flex flex-col flex-nowrap">
 
-        <!--HEADER-->
-        <div class="flex flex-row">
+<!--HEADER-->
+<div class="flex flex-row">
 
-            <!--PLM HEADER-->
-            <img class="h-[5.5rem] w-[5.5rem] ml-8 my-2" src="/images/Logo only.png">
-            <div class = " py-3 w-4/5 ml-5 flex flex-col">
-                <h1 class="text-[35px] font-katibeh mt-0 -mb-2 text-gold">
-                    PAMANTASAN NG LUNGSOD NG MAYNILA
-                </h1>
-                <h2 class="-mt-0 text-[15px] font-inter text-black-200">
-                    UNIVERSITY OF THE CITY OF MANILA
-                </h2>
-            </div>
-        </div>
+  <!--PLM HEADER-->
+    <button onclick="window.location.href='/'">
+    <img class="h-[4rem] w-[4rem] ml-8 my-2" src="/img/image-6.png">
+    </button>
+    <div class = "py-3 w-4/5 ml-4 flex flex-col">
+     <h1 class="text-[30px] font-katibeh mt-2 -mb-3 text-gold">
+        PAMANTASAN NG LUNGSOD NG MAYNILA
+     </h1>
+     <h2 class=" text-[15px] font-inter text-black-200">
+        UNIVERSITY OF THE CITY OF MANILA
+     </h2>
+     </div>  
 
-        <!--NAVBAR-->
+     <!--BUTTONS-->
+     <div class="flex flex-row -ml-[4rem]">
+    <button 
+      onclick="window.location.href='/alumni'" 
+      class="hover:bg-blue-hover duration-150 hover:border-blue-hover font-inter text-center text-16 whitespace-nowrap text-white-10 my-6 px-14 py-2 bg-blue rounded border-[1px] border-blue">
+      LOG IN
+    </button>
+  </div>
+</div>
+
+
+<!--NAVBAR-->
         <div class="bg-blue  h-1/5 w-screen">
             <navbar
                 class="text-bold text-white-30 text-paragraph3 py-[0.5rem] place-content-evenly font-inter w-screen flex flex-row">
@@ -46,7 +62,7 @@
                 </div>
 
               
-                <div class="relative flex flex-row" x-data="{ open: false }" @click.away="open = false">
+                <!-- <div class="relative flex flex-row" x-data="{ open: false }" @click.away="open = false">
                     <div>
                         <a href="#" onclick="window.location.href='/resources/views/careers.html'"
                             class ="hover:text-gold">
@@ -66,13 +82,15 @@
                             class="dropdown-menu-item hover:text-gold duration-150">Job Posting</div>
                     </div>
                 </div>
+             -->
+
                 <div class="relative flex flex-row" x-data="{ open: false }" @click.away="open = false">
                     <div>
-                        <a href="#" onclick="window.location.href='{{ url('') }}'" class ="hover:text-gold">
-                            ALUMNI EVENTS
+                        <a href="#" onclick="window.location.href='{{ url('/gallery1') }}'" class ="hover:text-gold">
+                            ALUMNI EVENT GALLERY
                         </a>
                     </div>
-                    <svg class="dropdown flex flex-col" width="18" height="18" viewBox="0 0 18 18" fill="none"
+                    <!-- <svg class="dropdown flex flex-col" width="18" height="18" viewBox="0 0 18 18" fill="none"
                         xmlns="http://www.w3.org/2000/svg" @click="open = !open">
                         <path :d="open ? 'M13.5 11.25L9 6.75L4.5 11.25' : 'M13.5 6.75L9 11.25L4.5 6.75'" stroke="white"
                             class="hover:fill-current hover:text-gold" />
@@ -83,7 +101,7 @@
                             class="dropdown-menu-item hover:text-gold duration-150">Gallery</div>
                         <div onclick="window.location.href='{{ url('event1') }}'"
                             class="dropdown-menu-item hover:text-gold duration-150">Upcoming Events</div>
-                    </div>
+                    </div> -->
                 </div>
             </navbar>
         </div>
@@ -98,6 +116,7 @@
                 window.location.href = url;
             }
         </script>
+
 
         <!--MAIN PAGE-->
         <div class="h-screen w-screen flex flex-col">
