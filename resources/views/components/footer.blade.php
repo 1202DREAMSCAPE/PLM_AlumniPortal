fd4c047a25177a031dad20f5ac6f4765b1cb32ec
MARIEEE0528 <mariecatugas1@gmail.com>
2024-06-04 23:45:48 +0800

    footer changes

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,11 +1,49 @@
 <!-- Footer -->
-<div class="relative w-screen py-8 flex justify-center items-center bg-black text-white">
-    <img class="absolute inset-0 w-full h-full object-cover z-0" src="/images/Footer.png" alt="Footer Background" />
-    <div class="relative flex flex-col items-center z-10">
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css">
+    <link rel="stylesheet" href="/dist/app.css">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.3/dist/cdn.min.js"></script>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Katibeh:wght@400;700&display=swap');
+    </style>
+</head>
+
+<div class="relative w-screen py-8 flex justify-center items-center text-white">
+    <div class="relative flex flex-col items-center z-5">
 
         <!-- CREDITS -->
-        <div class="text-md text-center opacity-75">
-            © 1967 - 2024 Pamantasan ng Lungsod ng Maynila. All Rights Reserved
+        <div class="w-screen py-32 text-center bg-blue-hover">
+            <div class="text-white-10 text-[64px] font-bold font-inter">
+                FOLLOW US ON SOCIAL MEDIA
+            </div>
+            <div class="opacity-60 pl-[10rem] ml-[20rem] mt-4 text-center w-1/2 text-[20px] text-white-10 font-inter">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.
+            </div>
         </div>
+
+        <!-- PHOTOS-->
+        <div class="w-screen px-[14rem] pb-[9rem] bg-blue-hover">
+            <div class="flex flex-row place-content-evenly place-content-center">
+                <img src="/img/Facebook.png">
+                <img src="/img/Twitter.png">
+                <img src="/img/Instagram.png">
+                <img src="/img/Linkedin.png">
+            </div>
+        </div>
+
     </div>
+</div>
 </div>
