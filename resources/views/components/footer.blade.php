2bfd3528942fa0e2a3a4e8e0a7e222302753565d
MARIEEE0528 <mariecatugas1@gmail.com>
2024-06-05 00:22:40 +0800

    footer changes

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,49 +1,29 @@
-<!-- Footer -->
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
-    </script>
-    <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css">
-    <link rel="stylesheet" href="/dist/app.css">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.3/dist/cdn.min.js"></script>
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
-        @import url('https://fonts.googleapis.com/css2?family=Katibeh:wght@400;700&display=swap');
-    </style>
-</head>
-
-<div class="relative w-screen py-8 flex justify-center items-center text-white">
-    <div class="relative flex flex-col items-center z-5">
+<section class="relative bg-black text-white py-8 flex justify-center items-center">
+    <img class="absolute inset-0 w-full h-full object-cover z-0" src="/images/Footer.png" alt="Footer Background" />
+    <div class="relative z-10 flex flex-col items-center">
 
         <!-- CREDITS -->
-        <div class="w-screen py-32 text-center bg-blue-hover">
-            <div class="text-white-10 text-[64px] font-bold font-inter">
-                FOLLOW US ON SOCIAL MEDIA
-            </div>
-            <div class="opacity-60 pl-[10rem] ml-[20rem] mt-4 text-center w-1/2 text-[20px] text-white-10 font-inter">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.
-            </div>
+
+        <div class="bg-blue-hover w-full py-40 text-center" style="padding: 1in;">
+            <h1 class="font-bold text-3xl md:text-3xl lg:text-3xl">FOLLOW US ON SOCIAL MEDIA</h1>
+            <p class="mt-8 text-xl md:text-2xl lg:text-3xl opacity-60 mx-auto max-w-screen-md" style="margin-top: 0.5cm;">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua.</p>
         </div>
 
         <!-- PHOTOS-->
-        <div class="w-screen px-[14rem] pb-[9rem] bg-blue-hover">
-            <div class="flex flex-row place-content-evenly place-content-center">
-                <img src="/img/Facebook.png">
-                <img src="/img/Twitter.png">
-                <img src="/img/Instagram.png">
-                <img src="/img/Linkedin.png">
+
+        <div class="bg-blue-hover w-full px-8 md:px-14 pb-16 md:pb-32">
+            <div class="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-4">
+                <img src="/img/Facebook.png" alt="Facebook"
+                    class="w-24 h-24 md:w-32 md:h-32 lg:w-40 lg:h-4 rounded-md shadow-md mx-auto my-4 md:my-0">
+                <img src="/img/Twitter.png" alt="Twitter"
+                    class="w-24 h-24 md:w-32 md:h-32 lg:w-40 lg:h-4 rounded-md shadow-md mx-auto my-4 md:my-0">
+                <img src="/img/Instagram.png" alt="Instagram"
+                    class="w-24 h-24 md:w-32 md:h-32 lg:w-40 lg:h-4 rounded-md shadow-md mx-auto my-4 md:my-0">
+                <img src="/img/Linkedin.png" alt="Linkedin"
+                    class="w-24 h-24 md:w-32 md:h-32 lg:w-40 lg:h-4 rounded-md shadow-md mx-auto my-4 md:my-0">
             </div>
         </div>
-
     </div>
-</div>
-</div>
+</section>
